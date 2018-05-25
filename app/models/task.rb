bc14ef0e68e6256d3e98cae5402a70a6ca602fd2
hasebe <hasebe@appirits.com>
2018-05-25 16:44:51 +0900

    バリデーション設定 優先順位を日本語化

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,7 +1,18 @@
 class Task < ApplicationRecord
 
   STATUS_NAME_MAP = { '未着手': 0, '作業中': 1, '完了': 2 }
+  PRIORITY_NAME_MAP = { '低': 0, '中': 1, '高': 2}
   enum status: STATUS_NAME_MAP
+  enum priority: PRIORITY_NAME_MAP
+  
+  validates :title,    presence: true
+  validates :status,   presence: true
+  validates :priority, presence: true
+  validate  :deadline_cannot_be_in_the_past, if: -> { deadline.present? }
+
+  def deadline_cannot_be_in_the_past
+    errors.add(:deadline, 'は現在日付以降の日時を設定してください。') if deadline < Time.current.beginning_of_day
+  end
   
   scope :order_list, lambda { |directions|
     priority_dir, deadline_dir = directions
