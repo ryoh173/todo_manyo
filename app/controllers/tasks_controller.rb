bf9480ffccd5571f29e62a898caa6a408b36a004
hasebe <hasebe@appirits.com>
2018-05-25 15:51:30 +0900

    ステータスで検索可能にした

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -71,7 +71,7 @@ class TasksController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def task_params
-      params.require(:task).permit(:title, :description, :deadline, :priority, :status, :q)
+      params.require(:task).permit(:title, :description, :deadline, :priority, :status, :q )
     end
     
     # ソート用 params[:s] = ['asc', 'asc']
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,8 @@
 class Task < ApplicationRecord
+
+  STATUS_NAME_MAP = { '未着手': 0, '作業中': 1, '完了': 2 }
+  enum status: STATUS_NAME_MAP
+  
   scope :order_list, lambda { |directions|
     priority_dir, deadline_dir = directions
     priority_dir, deadline_dir = [priority_dir, deadline_dir].map do |x|
@@ -7,4 +11,10 @@ class Task < ApplicationRecord
     scope = current_scope || all
     scope.order({priority: priority_dir}, {deadline: deadline_dir})
   }
+  
+  private
+  
+  ransacker :status, formatter: proc { |v| STATUS_NAME_MAP[v.to_sym] } do |parent|
+    parent.table[:status]
+  end
 end
